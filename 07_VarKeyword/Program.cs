e90f531e58770c659ffe51efea27391c90c0da82
Hakob Hovhannisyan <hakhov@gmail.com>
2022-08-31 21:52:38 +0400

    (_, age, _, hight) implimintation

--- 07_VarKeyword/Program.cs
+++ 07_VarKeyword/Program.cs
@@ -17,7 +17,13 @@ Console.WriteLine("-------------------");
 (int, double, string, char) yy = (10, 2.5, "Hello", 'c');
 var yy1 = (10, 2.5, "Hello", 'c');
 
-//var(_, _, _, pop1, _, pop2) = QueryCityDataForYears("New York City", 1960, 2010);
+var (_, age, _, hight) = GetInfo("New York City", 1960, 180);
+Console.WriteLine(age);
+
+(string, int, double, int) GetInfo(string v1, int v2, double v3)
+{
+    return (v1, v2, v3, 50);
+}
 
 (double color, double colorHEX) numbers = (12.5, 10.8); // test
 Console.WriteLine(numbers.color);
