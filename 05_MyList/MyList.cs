578cf26931a5da43eb76e5ca9ab1094a1f781183
Hakob Hovhannisyan <63061647+hakhov@users.noreply.github.com>
2022-11-16 22:06:39 +0400

    16.11.2022

---
+++ 05_MyList/MyList.cs
@@ -0,0 +1,87 @@
+﻿public class MyList<T>
+{
+    private T[] data;
+    private int size = 0;
+    private int capacity;
+
+    public MyList(int intialCapacity = 2)
+    {
+        if (intialCapacity < 1) intialCapacity = 1;
+
+        this.capacity = intialCapacity;
+        data = new T[intialCapacity];
+    }
+
+    public int Size { get { return size; } }
+    public bool IsEmpty { get { return size == 0; } }
+
+    public T GetAtElement(int index)
+    {
+        ThrowIfIndexOutOfRange(index);
+        return data[index];
+    }
+
+    public void SetAtElement(int index, T newElement)
+    {
+        ThrowIfIndexOutOfRange(index);
+        data[index] = newElement;
+    }
+
+    private void ThrowIfIndexOutOfRange(int index)
+    {
+        if (index > size - 1 || index < 0)
+            throw new ArgumentOutOfRangeException("This index iz not vaild");
+    }
+
+    public void Add(T item)
+    {
+        if (size == capacity)
+            Resize();
+
+        data[size++] = item;
+    }
+
+    private void Resize()
+    {
+        T[] resizedArray = new T[2 * capacity];
+        for (int i = 0; i < capacity; i++)
+        {
+            resizedArray[i] = data[i];
+        }
+        data = resizedArray;
+        capacity = capacity * 2;
+    }
+
+    public void InsertAtElement(int index, T newElement)
+    {
+        ThrowIfIndexOutOfRange(index);
+
+        if (size == capacity)
+            Resize();
+
+        for (int i = size; i > index; i--)
+        {
+            data[i] = data[i - 1];
+        }
+        data[2] = (T);
+
+        data[index] = newElement;
+        size++;
+    }
+
+    public void DeleteAtElement(int index)
+    {
+        //do implement
+    }
+
+    public bool Contains(T value)
+    {
+        //do implement
+        return false;
+    }
+
+    public void Clear()
+    {
+        // do implemenet
+    }
+}
---
+++ 05_MyList/Program.cs
@@ -0,0 +1,12 @@
+﻿MyList<int> ob = new MyList<int>();
+Console.WriteLine(ob.Size);
+
+ob.Add(1);
+ob.Add(2);
+ob.Add(3);
+Console.WriteLine(ob.GetAtElement(2));
+
+ob.InsertAtElement(2, -10);
+Console.WriteLine(ob.GetAtElement(2));
+
+Console.WriteLine(ob.Size);
