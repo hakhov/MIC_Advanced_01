895dbfeb41b0948c34f02ae63318926fa49ad1e5
Hakob Hovhannisyan <63061647+hakhov@users.noreply.github.com>
2022-11-23 22:04:37 +0400

    23.11.2022 LINQ

---
+++ 01_Linq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+string[] txt = { "Hakob", "Hayk", "Samvel", "David", "Asya", "Lilit" };
+
+var selectedString = new List<string>();
+
+foreach (string str in txt)
+{
+    if (str.ToUpper().StartsWith("H"))
+        selectedString.Add(str.ToUpper());
+}
+
+Console.WriteLine($"Selected Array: {string.Join(", ", selectedString)}");
+
+var selectedLinqARray = from item in txt
+                        where item.StartsWith("H")
+                        select item;
+
+var lmb = txt.Where(x => x.StartsWith("H")).OrderBy(x => x).ToList();
+
+Console.WriteLine($"Selected Array: {string.Join(", ", lmb)}");
