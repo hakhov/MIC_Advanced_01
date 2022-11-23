8cd5ad675e2b4fed8d562aff1661c38d0b922ae6
Hakob Hovhannisyan <63061647+hakhov@users.noreply.github.com>
2022-11-23 21:49:41 +0400

    23.11.2022

---
+++ QuickSort/MyProgram.cs
@@ -0,0 +1,40 @@
+﻿
+public partial class Program
+{
+    public static int[] QuickSort(int[] arr, int firstIndex, int lastIndex)
+    {
+        if (firstIndex >= lastIndex)
+            return arr;
+
+        int pivot = GetPivot(arr, firstIndex, lastIndex);
+        QuickSort(arr, firstIndex, pivot - 1);
+        QuickSort(arr, pivot + 1, lastIndex);
+
+        return arr;
+    }
+
+    private static int GetPivot(int[] arr, int firstIndex, int lastIndex)
+    {
+        int pivot = firstIndex - 1;
+
+        for (int i = firstIndex; i <= lastIndex; i++)
+        {
+            if (arr[i] < arr[lastIndex])
+            {
+                pivot++;
+                Swap(ref arr[pivot], ref arr[i]);
+            }
+        }
+        pivot++;
+        Swap(ref arr[pivot], ref arr[lastIndex]);
+
+        return pivot;
+    }
+
+    private static void Swap(ref int leftElement, ref int rightElement)
+    {
+        int temp = leftElement;
+        leftElement = rightElement;
+        rightElement = temp;
+    }
+}
---
+++ QuickSort/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] arr = { -1, 85, 90, 5, 13, 5, 2, 20, 1, 8, -9, 10, -50 };
+int firstIndex = 0;
+int lastIndex = arr.Length - 1;
+
+int[] sortedArray = QuickSort(arr, firstIndex, lastIndex);
+
+
+Console.WriteLine($"Sorted Array: {string.Join(", ", sortedArray)}");
+
